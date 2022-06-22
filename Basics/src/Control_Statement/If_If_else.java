62adc89b6b5edff12c8b6c2a42e514c66a99c043
Sathisha Narayana <Sathisha Narayana@Sathisha>
2022-06-22 10:28:58 +0530

    Basic If and If-else program

---
+++ Basics/src/Control_Statement/If_If_else.java
@@ -0,0 +1,27 @@
+package Control_Statement;
+
+public class If_If_else {
+
+	public static void main(String[] args) {
+		int flag = 1;
+		if(flag == 1)
+		{
+			System.out.println("System Start");
+		}
+		else{
+			System.out.println("System Stop");
+			
+		}
+		flag = 0;
+		if(flag == 1)
+		{
+			System.out.println("System Start");
+		}
+		else{
+			System.out.println("System Stop");
+			
+		}
+
+	}
+
+}
