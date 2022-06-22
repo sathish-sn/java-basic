c627abd6f22384b41210373c6a8404fd94f8fa41
Sathisha Narayana <Sathisha Narayana@Sathisha>
2022-06-22 11:16:25 +0530

    Interview Question

---
+++ Basics/src/Control_Statement/InterviewQ.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @author Sathisha Narayana
+ * 
+ * Question -In a parking system you are charged 20rs upto 2 hour  
+ * after that for every 1 hour you will be charge additional 20rs 
+ * 
+ */
+package Control_Statement;
+
+public class InterviewQ {
+
+	public static void main(String[] args) {
+
+		int b_charge = 20; //Base fare
+		int hour = 3; 
+		int final_charge;
+		
+			if(hour<2)
+			{
+				System.out.println("Your Parking Charge is = 20 ");
+			}else {
+			final_charge = (hour-1)*b_charge;
+			System.out.println("Your Parking Charge is = " + final_charge);
+			}
+	}
+
+}
