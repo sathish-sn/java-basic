98b4a491cd272f5f25fd830670909adb5d22af3b
Sathisha Narayana <Sathisha Narayana@Sathisha>
2022-06-22 11:27:04 +0530

    interview Question

--- Basics/src/Control_Statement/Question.java
+++ Basics/src/Control_Statement/Question.java
@@ -21,7 +21,9 @@ public class Question {
 			}else {
 			final_charge = (hour-1)*b_charge;
 			System.out.println("Your Parking Charge is = " + final_charge);
+			System.out.print("*********************U");
 			}
+			
 	
 
 }
